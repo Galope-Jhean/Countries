9db76a9560e3306c1283b98e98fba89584f53202
Jhean Khendrick <drakekhendrick@gmail.com>
2023-09-10 01:19:22 +0800

    finished all states and added media queries

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,9 +1,12 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useState, useContext } from "react";
+import { Link, useParams } from "react-router-dom";
+import { AppData } from "../App";
 
 const Details = () => {
     const [country, setCountry] = useState({});
+    const [data, setData] = useState([]);
     const { name } = useParams();
+    const {darkMode} = useContext(AppData)[0];
 
     useEffect(() => {
         fetch(`https://restcountries.com/v3.1/name/${name}`)
@@ -11,6 +14,14 @@ const Details = () => {
             .then(data => setCountry(data[0]))
             .catch(error => console.error(error));
     }, [name]);
+
+    useEffect(() => {
+      fetch(` https://restcountries.com/v3.1/all`)
+      .then(response => response.json())
+      .then(json => setData(json))
+      .catch(error => console.log(error))
+    }, [])
+
 
     if (!country) {
         return <p>Loading...</p>;
@@ -29,6 +40,27 @@ const Details = () => {
         borders
     } = country || {};
 
+    let formattedBorders = [];
+
+    if (borders) {
+      data.forEach((country) => {
+        borders.forEach((countryBorder) => {
+          if (countryBorder === country.cioc) {
+            formattedBorders.push(country.name.common);
+          }
+        });
+      });
+    }
+
+    const borderList = formattedBorders.map((border, index) => (
+      <Link 
+        key={index} 
+        className={darkMode ? "darkModeElement border-link" : "border-link" }
+        to={`/countries/${border}`}>
+          {border}
+      </Link>
+    ))
+
     let currencyNames = "";
 
     if (typeof currencies === "object") {
@@ -45,7 +77,7 @@ const Details = () => {
     : null;
   
     const languageFormat = languageList ? languageList.join(", ") : '';
-    console.log(borders)
+  
     return (
       <div className="details-container">
         <div className="country-flag">
@@ -53,17 +85,25 @@ const Details = () => {
         </div>
         <div className="country-details">
           <h2>{common}</h2>
-          <span><strong>Population: </strong>{population?.toLocaleString("en-US")}</span>
-          <span><strong>Region: </strong>{region}</span>
-          <span><strong>Subregion: </strong>{subregion}</span>
-          <span><strong>Capital: </strong>{capital}</span>
+          <div className="overall-details">
+            <div className="toplevel-details">
+              <span><strong>Population: </strong>{population?.toLocaleString("en-US")}</span><br />
+              <span><strong>Region: </strong>{region}</span><br />
+              <span><strong>Subregion: </strong>{subregion}</span><br />
+              <span><strong>Capital: </strong>{capital}</span><br />
+            </div>
+            <span className="newSection"></span>
+            <div className="secondlevel-details">
+              <span><strong>Top Level Domain: </strong>{topLevelDomain}</span><br />
+              <span><strong>Currencies: </strong>{currencyNames}</span><br />
+              <span><strong>Languages: </strong>{languageFormat}</span><br />
+            </div>
+          </div>
           <span className="newSection"></span>
-          <span><strong>Top Level Domain: </strong>{topLevelDomain}</span>
-          <span><strong>Currencies: </strong>{currencyNames}</span>
-          <span><strong>Languages: </strong>{languageFormat}</span>
-          <span><strong>Borders: </strong>{borders ? borders.join(", ") : 'None'}</span>
-        </div>
-        <div className="borderLink">
+          <span><strong>Border Countries: </strong></span>
+          <div className="border-container">
+              {formattedBorders.length > 0 ? borderList : 'None'}
+          </div>
         </div>
       </div>
     );
